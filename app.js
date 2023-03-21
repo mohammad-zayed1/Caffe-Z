d12d72b5330ade59cedc68b4f565fbb41ff10bb8
mohammad zayed <mohammadzayed193@gmail.com>
2023-03-21 09:10:32 +0300

    edit

--- app.js
+++ app.js
@@ -10,25 +10,25 @@
         
             userGender = prompt(" enter the correct gender");
          }
+         if(userGender === 'male'){
+            alert("Welcome Mr." + userName);
+            
+        }else if(userGender === 'female'){
+            alert("Welcome Ms." + userName);
+            
+        }else{
+            alert("Welcome")
+        }
     }
     gender();
 
-    if(userGender === 'male'){
-        alert("Welcome Mr." + userName);
-        
-    }else if(userGender === 'female'){
-        alert("Welcome Ms." + userName);
-        
-    }else{
-        alert("Welcome")
-    }
+   
     const userOrderType = prompt("What do you want hot or cold drink ?");
     const userDrinkName = prompt("Plz write the name of your drink ?");
     alert("Your drink is getting prepared ");
     
     let info = [userName , userGender , userOrderType , userDrinkName];
     
-    let text = '';
     for (let i = 0; i < info.length; i++){
         console.log(info[i]);
     }
