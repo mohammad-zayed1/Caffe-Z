62646a6ed2f8f32cd8f89b449b77ee2f8315d4b9
mohammad zayed <mohammadzayed193@gmail.com>
2023-03-21 15:37:13 +0300

    practice on dom

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 
 
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     const userName = prompt(" what is your name ?");
     let userGender = prompt(" what is your Gender ?");
+    let userAge = prompt(" what is your Age ?");
     
   
     function gender(){
@@ -29,6 +55,36 @@
     for (let i = 0; i < info.length; i++){
         console.log(info[i]);
     }
+
+
+
+
+    let userInfo = document.getElementById("userInfo");
+
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    let ul = document.createElement("ul");
+    let li1 = document.createElement("li");
+    let li2 = document.createElement("li");
+    let li3 = document.createElement("li");
+
+    p.textContent = `${userName}`;
+    li1.textContent=`Gender : ${userGender}`;
+    li2.textContent=`Age : ${userAge}`;
+    li3.textContent=`Drink : ${userOrderType} ${userDrinkName}`;
+     
+
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+
+    div.appendChild(p);
+    div.appendChild(ul);
+
+    userInfo.appendChild(div);
+    
+    
+
 
 
 
